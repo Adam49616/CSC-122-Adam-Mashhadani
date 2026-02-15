5258f251087908ff70c822491e3b70dc2841be2e
Adam49616 <aa49616@mail.harpercollege.edu>
2026-02-15 17:05:40 -0600

    Add files via upload

---
+++ CSC 122- Hello World Lab.cpp
@@ -0,0 +1,14 @@
+/*
+Adam Mashhadani
+2/15/2026
+Project: Hello World Lab
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    return 0;  // exits program gracefully
+}
+
