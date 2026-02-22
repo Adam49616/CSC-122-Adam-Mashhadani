14cb1665188acdbc9a0080cf7264d2877d27ca88
Adam49616 <aa49616@mail.harpercollege.edu>
2026-02-22 16:13:02 -0600

    Delete Find.h

--- Find.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-// find character in string
-int find(const std::string& text, char ch);
-
-// find substring in string (overloaded)
-int find(const std::string& text, const std::string& sub);
