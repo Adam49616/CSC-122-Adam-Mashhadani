1e9940f9506a867838ad1b4d986c97de1016113c
Adam49616 <aa49616@mail.harpercollege.edu>
2026-03-01 13:20:15 -0600

    Oops! Shall We Try Again Lab

--- Main.cpp
+++ Main.cpp
@@ -1,50 +1,22 @@
 /*
 Adam Mashhadani
-2/22/2026
-Project: It's Raining Strings Lab
+3/1/2026
+Project: Oops! Shall We Try Again Lab
 */
 
-
 #include <iostream>
-#include <vector>
-#include <string>
-#include "Busniess.h"
+#include "validate.h"
 
-using namespace std;
+int main()
+{
+    int result = getValidatedInt(
+        0, 100,
+        "Please enter a value (0-100):",
+        "Your value is invalid"
+    );
 
-int main() {
-    vector<string> businesses;
-    string input;
-    string response;
-    string lowerResponse;
-
-    cout << "Welcome to the Business Sorting Program!" << endl << endl;
-
-    do {
-        cout << "Please enter the name of a business:  ";
-        getline(cin, input);
-
-        addBusiness(businesses, input);
-
-        cout << endl;
-        if (businesses.size() == 1)
-            cout << "Your business is:" << endl << endl;
-        else
-            cout << "Your businesses are:" << endl << endl;
-
-        for (const string& name : businesses)
-            cout << "    " << name << endl;
-
-        cout << endl << "Another business?  ";
-        getline(cin, response);
-
-        lowerResponse.clear();
-        for (char c : response)
-            lowerResponse += tolower(c);
-
-    } while (lowerResponse == "y" || lowerResponse == "yes");
-
-    cout << endl << "Thank you for using the Business Sorting Program!" << endl;
+    std::cout << "The value chosen by the user is "
+        << result << std::endl;
 
     return 0;
 }
